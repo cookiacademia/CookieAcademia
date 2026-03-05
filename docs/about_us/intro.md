0ff417541d2e616ecee985f0c560e7beb3ceb352
Nixarr <nixarrow1@gmail.com>
2026-03-05 03:27:08 +0100

    Main page enhancement

--- docs/about_us/intro.md
+++ docs/about_us/intro.md
@@ -1,1 +1,20 @@
 # A propos de nous
+
+## Qui somme nous ?
+
+Nous sommes un groupe d’étudiants issus de différents parcours académiques, réunis par une même ambition : partager nos connaissances, transmettre nos passions et rendre le savoir plus accessible à tous.
+
+Nos membres proviennent de disciplines variées et apportent chacun leur propre regard sur le monde académique. Cette diversité constitue la richesse du projet, car elle permet d’aborder les sujets sous différents angles et de favoriser les liens entre les domaines de connaissance.
+
+## Pourquoi Cookie Academia ?
+
+Cookie Academia est un projet de bibliothèque numérique dédié au partage et à la transmission du savoir.  
+Dans un monde où les disciplines sont souvent cloisonnées et les ressources dispersées sur de nombreuses plateformes, il devient difficile de trouver un espace centralisé permettant d'explorer librement différents domaines de connaissance.
+
+Notre objectif est donc de créer un lieu unique où la curiosité intellectuelle peut s'exprimer pleinement. Cookie Academia rassemble des contenus rédigés par des passionnés et des étudiants issus de divers horizons : mathématiques, informatique, physique, histoire, médecine, et bien d'autres disciplines. Chaque contribution vise à rendre le savoir plus accessible, structuré et agréable à explorer.
+
+Notre ambition est de proposer un espace où la découverte intellectuelle ne s'arrête jamais, et où chacun peut venir apprendre quelque chose de nouveau.
+
+## Pourquoi Cookie ?
+
+Parce que c'est bon !
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,46 +4,38 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: 'Mathématiques',
+    link: 'docs/math/intro',
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    title: 'Informatique',
+    link: 'docs/computer_science/intro',
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: 'Histoire',
+    link: 'docs/history/intro',
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({title, link}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+        <a
+          href={link}
+          style={{
+            fontSize: '1.8rem',
+            padding: '20px 40px',
+            borderRadius: '12px',
+            background: '#a128f2',
+            color: 'white',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontWeight: 'bold',
+          }}
+        >
+          {title}
+        </a>
       </div>
     </div>
   );
